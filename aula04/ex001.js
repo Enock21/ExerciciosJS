6bfcefc58e2359837b1921c9a46ff5efc8e26027
Enock Bezerra Ferreira de Souza <enock.souza@ccc.ufcg.edu.br>
2023-02-21 11:27:24 -0300

    Modificando estrutura do ex001
    Foram separados os arquivos .css e .js do .html e foram adicionadas referências aos 2 primeiros no último.

---
+++ aula04/ex001.js
@@ -0,0 +1,3 @@
+window.alert('Hey, listen!') //Comando para emitir uma janela de alerta na tela. Também pode ser usada como: alert('Hey, listen!')
+window.confirm('Just press OK. No questions asked.') //Comando para exibir uma janela de confirmação
+window.prompt('Wait, who the hell are you!?') //Comando para exibir uma janela de inserção de texto
