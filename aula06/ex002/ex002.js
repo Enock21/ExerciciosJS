172a3b47f1dbae414c3cdfe90cc6f9c513bf9c02
Enock Bezerra Ferreira de Souza <enock.souza@ccc.ufcg.edu.br>
2023-02-21 11:42:30 -0300

    Estrutura do ex002 alterada
    Mesma mudança feita com o ex001 no último commit.

---
+++ aula06/ex002/ex002.js
@@ -0,0 +1,3 @@
+var nome = window.prompt('Wait, who the hell are you!?') //Comando para exibir uma janela de inserção de texto. Graças a var nome, o retorno do usuário pode ser armazenado
+
+window.alert('Alright. Now get outta here ' + nome + '!!') //Exibe uma janela com o texto descrito e o valor armazenado na variável nome
